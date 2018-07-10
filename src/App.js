66ac995b26967db8b2e233de31b070f37e9e6b00
Jordão Macedo <jodao05@hotmail.com>
2018-07-10 08:35:27 -0300

    Add common components

--- src/App.js
+++ src/App.js
@@ -2,10 +2,24 @@ import React, { Component } from 'react';
 import {  View, Text, } from 'react-native';
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import firebase from 'firebase';
 
 import reducers from "./reducers";
 
 class App extends Component {
+
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyDlUtpkFIoaUlIoRnSdxX6cxsHtbMGzNDY",
+      authDomain: "manager-dbad0.firebaseapp.com",
+      databaseURL: "https://manager-dbad0.firebaseio.com",
+      projectId: "manager-dbad0",
+      storageBucket: "manager-dbad0.appspot.com",
+      messagingSenderId: "999914894879"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
